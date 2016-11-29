4ee45cab1cc28ccbe550f99a5c0d8bdfcc454804
Jeff Doan <jeffdoan@jeffdoan.local>
2016-11-29 15:58:57 -0800

    Updates

--- cards/js/app.js
+++ cards/js/app.js
@@ -6,12 +6,21 @@ $(function(){
 		delayUX(hiddenCardsInd, $(this));
 	});
 
+	// $('.dv-help-card').on('click', function(){
+	// 	delayClickUX($(this));
+	// });
+
 	// $('.dv-help-card').hover(function(){
 	// 	$(this).addClass('focus');
 	// }, function(){
-	// 	$(this).removeClass('focus');
-	// 	$(this).blur();
+	// 	$(this).removeClass('focus').blur();
 	// });
+
+	// function delayClickUX(el) {
+	// 	setTimeout(function() {
+	// 		el.addClass('clicked').blur();
+	// 	}, 100);
+	// }
 
 	function delayUX(el1, el2) {
 		setTimeout(function() {
