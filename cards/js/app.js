aeec5fb3ee64f83a9801eaf7bc011a125864dc8a
Jeff Doan <jeffdoan@jeffdoan.local>
2016-11-29 13:28:56 -0800

    Updates

---
+++ cards/js/app.js
@@ -0,0 +1,17 @@
+$(function(){
+	var hiddenCardShow = $('#dv-help-cards-show'),
+		hiddenCardsInd = $('.hide');
+
+	hiddenCardShow.on('click', function(){
+		delayUX(hiddenCardsInd, $(this));
+	});
+
+	function delayUX(el1, el2) {
+		setTimeout(function() {
+			el1.each(function(){
+				$(this).removeClass('hide');
+			});
+			el2.remove();
+		}, 215);
+	}	
+});
