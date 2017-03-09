f50559a69118289f9947664a32a97917dd1fba56
Jeff Doan <jeffdoan@jeffdoan.local>
2017-03-08 16:24:22 -0800

    Updates

--- typeface/js/app.js
+++ typeface/js/app.js
@@ -1,5 +1,6 @@
 $(function() {
 	$('#dv-floating-button').on('click', function() {
+		$(this).hide();
 		$('#dv-list').addClass('shown');
 	});
 	$('span', '#dv-list').on('click', function() {
@@ -9,5 +10,7 @@ $(function() {
 		$(this).addClass('active');
 		$('html').removeAttr('class').addClass($(this).attr('class'));
 		$('#dv-list').removeClass('shown');
+		$('#dv-floating-button').show();
+		window.scroll(0,0);
 	});
 });
