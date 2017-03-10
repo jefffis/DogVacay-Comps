9e54d3cb249a149216a0b3a9361745226fc76a36
Jeff Doan <jeffdoan@jeffdoan.local>
2017-03-10 12:45:39 -0800

    Add in whitney

--- typeface/js/app.js
+++ typeface/js/app.js
@@ -1,16 +1,47 @@
 $(function() {
+	var scrollPos;
 	$('#dv-floating-button').on('click', function() {
 		$(this).hide();
 		$('#dv-list').addClass('shown');
+		scrollPos = $(window).scrollTop();
+		hideUi();
 	});
 	$('span', '#dv-list').on('click', function() {
 		if($(this).hasClass('active')) return;
+
+		if($(this).hasClass('close')) {
+			$('#dv-list').removeClass('shown');
+			$('#dv-floating-button').show();
+			return;
+		}
 		
 		$('span', '#dv-list').removeClass('active');
 		$(this).addClass('active');
 		$('html').removeAttr('class').addClass($(this).attr('class'));
-		$('#dv-list').removeClass('shown');
-		$('#dv-floating-button').show();
-		window.scroll(0,0);
+		$('#dv-list').removeClass('rel');
+		$('.hidable').show();
+		showUi();
 	});
+	$('.close', '#dv-list').on('click', function() {
+		$('#dv-list').removeClass('rel');
+		$('.hidable').show();
+		showUi();
+	});
+	function hideUi() {
+		setTimeout(function() {
+			$('.hidable').hide();
+			$('#dv-list').addClass('rel');
+			$('body').addClass('list-shown');
+		}, 251);
+	}
+	function showUi() {
+		$('body').removeClass('list-shown');
+		setTimeout(function() {
+			window.scroll(0, scrollPos);
+			$('#dv-list').removeClass('shown');
+		}, 0);
+		setTimeout(function() {
+			$('#dv-floating-button').show();
+		}, 251);
+	}
 });
